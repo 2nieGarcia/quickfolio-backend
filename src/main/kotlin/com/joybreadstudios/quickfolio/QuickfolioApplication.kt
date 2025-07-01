0ea869c633c13381b4ac08ac5c17da277c0eee0b
Antonio Garcia <garciaantoniojrb@gmail.com>
2025-07-01 12:53:26 +0800

    inital commit

---
+++ src/main/kotlin/com/joybreadstudios/quickfolio/QuickfolioApplication.kt
@@ -0,0 +1,11 @@
+package com.joybreadstudios.quickfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QuickfolioApplication
+
+fun main(args: Array<String>) {
+	runApplication<QuickfolioApplication>(*args)
+}
---
+++ src/test/kotlin/com/joybreadstudios/quickfolio/QuickfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joybreadstudios.quickfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QuickfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
